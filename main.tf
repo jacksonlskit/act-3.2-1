4f540617c816a0faeddeddf0eceb66b48e4b0006
jacksonlskit <jack_sk0919@hotmail.com>
2026-09-23 20:52:04 +0800

    add s3 bucket

---
+++ main.tf
@@ -0,0 +1,15 @@
+terraform {
+  backend "s3" {
+    bucket       = "sctp-tfstate-ce13"
+    key          = "jack/act-3.2-1/terraform.tfstate"
+    region       = "us-east-1"
+  }
+}
+
+provider "aws" {
+   region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "workshop" {
+  bucket_prefix = "jack-cicd-workshop-"
+}
