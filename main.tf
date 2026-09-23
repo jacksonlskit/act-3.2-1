0ae0a462617be2eaffd7bb73035d02a23d0934cf
jacksonlskit <jack_sk0919@hotmail.com>
2026-09-23 22:14:42 +0800

    fixed tflint

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 terraform {
+  required_version = ">= 1.5.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
   backend "s3" {
     bucket = "sctp-tfstate-ce13"
     key    = "jack/act-3.2-1/terraform.tfstate"
